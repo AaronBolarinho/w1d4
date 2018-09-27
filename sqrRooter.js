3b9b38aacbd704d40069ddee83952d2073ea1c39
vagrant <aaron.bolarinho@gmail.com>
2018-09-27 16:25:55 +0000

    square Rooter assignment

--- sqrRooter.js
+++ sqrRooter.js
@@ -3,7 +3,8 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map (
+  x => Math.sqrt((x.x * x.x) + (x.y * x.y)))
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
